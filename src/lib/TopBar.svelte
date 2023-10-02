f8258fb22904f654177ecea7dc1f4abe52cb5dcf
Marco Moroni <marcomoroni394@gmail.com>
2023-10-02 11:52:57 +0200

    Mobile nav bar

--- src/lib/TopBar.svelte
+++ src/lib/TopBar.svelte
@@ -7,6 +7,8 @@
 	export let background = false;
 	export let showLogo = true;
 
+	let expanded = false;
+
 	const pages = [
 		{
 			href: '/works',
@@ -21,18 +23,20 @@
 			label: 'Contacts'
 		}
 	];
-
-	$: absolutePosClass = absolutePos ? 'abs' : '';
-	$: backgroundClass = background ? 'bg' : '';
 </script>
 
-<div class="container {absolutePosClass} {backgroundClass}">
+<div class="container" class:expanded class:abs={absolutePos} class:bg={background}>
 	<WidthContainer>
 		<nav class="top-bar">
+			<button
+				class="nav-bar-toggle style-like-a"
+				on:click={() => {
+					expanded = !expanded;
+				}}>Menu</button
+			>
 			{#each pages as page}
-				<a href={page.href}>{page.label}</a>
+				<a class="nav-link" href={page.href}>{page.label}</a>
 			{/each}
-			<div class="spacer" />
 			{#if showLogo}
 				<a transition:fade={{ duration: 100 }} class="home-link" href="/"><MxtLogoNoPadding /></a>
 			{/if}
@@ -48,30 +52,74 @@
 
 	.top-bar {
 		display: block;
-		height: 60px;
+		padding-top: 19px;
+		padding-bottom: 22px;
 		display: flex;
 		flex-direction: row;
 		align-items: center;
 		justify-content: start;
 		gap: 30px;
+		position: relative;
 	}
 
-	.spacer {
-		flex: 1;
+	.nav-bar-toggle {
+		display: none;
+	}
+
+	.nav-bar-toggle::after {
+		transform: translateY(-35%) rotate(45deg) scale(1);
+		border-bottom: 1.5px solid var(--color-primary);
+		border-right: 1.5px solid var(--color-primary);
+		content: '';
+		display: inline-block;
+		height: 8px;
+		margin: 0 2px 0 8px;
+		vertical-align: middle;
+		width: 8px;
+	}
+
+	.expanded .nav-bar-toggle::after {
+		transform: translateY(1px) rotate(225deg) scale(1);
 	}
 
 	.home-link {
-		/* Visual adjustement. */
-		margin-top: 3px;
-
+		position: absolute;
+		right: 0;
+		top: 19px;
 		width: 70px;
 	}
 
-	.abs {
+	:not(.expanded).abs {
 		position: absolute;
 	}
 
 	.bg {
 		background-color: #ede9e6;
+	}
+
+	@media (max-width: 700px) {
+		.container.expanded {
+			/* Force color. */
+			background-color: #ede9e6;
+		}
+
+		.top-bar {
+			flex-direction: column;
+			height: unset;
+			align-items: flex-start;
+			gap: 19px;
+		}
+
+		.nav-bar-toggle {
+			display: block;
+		}
+
+		.nav-link {
+			display: none;
+		}
+
+		.expanded .nav-link {
+			display: block;
+		}
 	}
 </style>
