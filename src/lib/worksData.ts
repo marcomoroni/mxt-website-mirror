69407ff2658e71426dc8498ce968e47b1afbcc62
Marco Moroni <marcomoroni394@gmail.com>
2023-10-06 11:32:17 +0200

    less repeating code

--- src/lib/worksData.ts
+++ src/lib/worksData.ts
@@ -1,32 +1,8 @@
-export const worksData = [
+export const worksData = Array(5).fill(
     {
         title: 'Visualising the Stonehenge World Heritage site landscape',
         href: '/case-studies/stonehenge',
         leadParagraph: 'Harnessing the power of immersive technologies for complex infrastructure projects',
         imageUrl: '/images/stonehenge.jpg',
-    },
-    {
-        title: 'Visualising the Stonehenge World Heritage site landscape',
-        href: '/case-studies/stonehenge',
-        leadParagraph: 'Harnessing the power of immersive technologies for complex infrastructure projects',
-        imageUrl: '/images/stonehenge.jpg',
-    },
-    {
-        title: 'Visualising the Stonehenge World Heritage site landscape',
-        href: '/case-studies/stonehenge',
-        leadParagraph: 'Harnessing the power of immersive technologies for complex infrastructure projects',
-        imageUrl: '/images/stonehenge.jpg',
-    },
-    {
-        title: 'Visualising the Stonehenge World Heritage site landscape',
-        href: '/case-studies/stonehenge',
-        leadParagraph: 'Harnessing the power of immersive technologies for complex infrastructure projects',
-        imageUrl: '/images/stonehenge.jpg',
-    },
-    {
-        title: 'Visualising the Stonehenge World Heritage site landscape',
-        href: '/case-studies/stonehenge',
-        leadParagraph: 'Harnessing the power of immersive technologies for complex infrastructure projects',
-        imageUrl: '/images/stonehenge.jpg',
-    },
-];
+    }
+);
