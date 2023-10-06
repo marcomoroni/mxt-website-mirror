6594e35e4f3991f0c8581169603e39ff1ab4c602
Marco Moroni <marcomoroni394@gmail.com>
2023-10-06 11:11:33 +0200

    remove leftover

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
