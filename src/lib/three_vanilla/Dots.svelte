a92db18a46e8c13e07aa745526965d5fd9079d34
Marco Moroni <marcomoroni394@gmail.com>
2023-11-06 15:41:39 +0100

    aniamation

--- src/lib/three_vanilla/Dots.svelte
+++ src/lib/three_vanilla/Dots.svelte
@@ -1,40 +1,52 @@
 <script lang="ts">
 	// Based on https://github.com/mrdoob/three.js/blob/master/examples/webgl_points_waves.html
 
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, onDestroy } from 'svelte';
 	import * as THREE from 'three';
 	import { ImprovedNoise } from 'three/examples/jsm/math/ImprovedNoise.js';
 	import { noise3D } from './shader_utils/noise-3d';
+	import { spring, type Unsubscriber } from 'svelte/motion';
+	import { map } from './shader_utils/map';
 
 	const dispatch = createEventDispatcher();
 
 	const vertexShader = `
 uniform float time;
+uniform float animRadius;
 attribute vec2 dotIndex;
-varying vec3 vColor;
+attribute float distanceFromCenter;
+out float vAnim;
 
 ${noise3D}
 
+${map}
+
 void main() {
+
+	float anim0to1 = clamp( animRadius / distanceFromCenter, 0.0, 1.0 );
 
 	float posXNoise = cnoise(vec3(dotIndex.r * 0.1 + 3.0, dotIndex.g * 0.1 - 6.0, time * 0.00006));
 	float posYNoise = cnoise(vec3(dotIndex.r * 0.1 + 80.0, dotIndex.g * 0.1 - 30.0, time * 0.00006));
-	float posX = position.r + posXNoise * 700.0;
-	float posY = position.g + posYNoise * 700.0;
+	float posX = position.r + mix(0.0, posXNoise * 900.0, anim0to1);
+	float posY = position.g + mix(0.0, posYNoise * 900.0, anim0to1);
 	vec4 mvPosition = modelViewMatrix * vec4( posX, posY, 0.0, 1.0 );
 
 	float scaleNoise = cnoise(vec3(dotIndex.r * 0.07, dotIndex.g * 0.07, time * 0.0003));
-	gl_PointSize = scaleNoise * 50.0;
+	scaleNoise = map(scaleNoise, -1.0, 1.0, -0.4, 1.0);
+
+	gl_PointSize = mix(200.0, scaleNoise * 60.0, anim0to1);
 
 	gl_Position = projectionMatrix * mvPosition;
 
-	vColor = color;
+	vAnim = anim0to1;
 }
 	`;
 
 	const fragmentShader = `
 uniform float time;
-varying vec3 vColor;
+uniform float animRadius;
+uniform vec3 baseColor;
+in float vAnim;
 
 ${noise3D}
 	
@@ -42,14 +54,19 @@ void main() {
 	
 	if ( length( gl_PointCoord - vec2( 0.5, 0.5 ) ) > 0.475 ) discard;
 	
-	gl_FragColor = vec4( vColor.rgb, 1.0 );
+	gl_FragColor = vec4( baseColor.rgb, 1.0 );
 
 	#include <tonemapping_fragment>
 	#include <colorspace_fragment>
 }
 `;
 
+	let unsubscribe: Unsubscriber | undefined = undefined;
+
 	function initScene(el: HTMLElement) {
+		// Values for animations.
+		const animRadius = spring(0, { stiffness: 0.002 });
+
 		const SEPARATION = 140,
 			AMOUNTX = 30,
 			AMOUNTY = 30;
@@ -94,7 +111,9 @@ void main() {
 
 		const dotIndex = new Float32Array(numParticles * 2);
 		const positions = new Float32Array(numParticles * 3);
-		const colors = new Float32Array(numParticles * 3);
+		const distancesFromCenter = new Float32Array(numParticles);
+
+		let maxDistanceFromCenter = 0;
 
 		let i = 0,
 			j = 0,
@@ -109,9 +128,9 @@ void main() {
 				positions[i + 1] = iy * SEPARATION - (AMOUNTY * SEPARATION) / 2; // y
 				positions[i + 2] = 0; // z
 
-				colors[i] = baseColor.r;
-				colors[i + 1] = baseColor.g;
-				colors[i + 2] = baseColor.b;
+				const distanceFromCenter = Math.hypot(positions[i], positions[i + 1]);
+				distancesFromCenter[j] = distanceFromCenter;
+				if (distanceFromCenter > maxDistanceFromCenter) maxDistanceFromCenter = distanceFromCenter;
 
 				i += 3;
 				p += 2;
@@ -122,11 +141,13 @@ void main() {
 		const geometry = new THREE.BufferGeometry();
 		geometry.setAttribute('dotIndex', new THREE.BufferAttribute(dotIndex, 2));
 		geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-		geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+		geometry.setAttribute('distanceFromCenter', new THREE.BufferAttribute(distancesFromCenter, 1));
 
 		const material = new THREE.ShaderMaterial({
 			uniforms: {
-				time: { value: timeSinceStart }
+				time: { value: timeSinceStart },
+				animRadius: { value: animRadius },
+				baseColor: { value: baseColor }
 			},
 			vertexShader,
 			fragmentShader,
@@ -187,44 +208,24 @@ void main() {
 			camera.position.y += (-mouseY - camera.position.y) * 0.05;
 			camera.lookAt(scene.position);
 
-			const positions = particles.geometry.attributes.position.array;
-			const colors = particles.geometry.attributes.color.array;
-
-			let i = 0,
-				j = 0;
-
 			timeSinceStart = new Date().getTime() - startTime;
-
-			for (let ix = 0; ix < AMOUNTX; ix++) {
-				for (let iy = 0; iy < AMOUNTY; iy++) {
-					const accentColorNoise = noise.noise(
-						ix * 0.8 + 30,
-						iy * 0.8 + 230,
-						timeSinceStart * 0.0002
-					);
-
-					const color = new THREE.Color().lerpColors(
-						baseColor,
-						accentColors[0],
-						clamp(mapRange(mapRange(accentColorNoise, -1, 1, 0, 1), 0.8, 0.82, 0, 1), 0, 1)
-					);
-					colors[i] = color.r;
-					colors[i + 1] = color.g;
-					colors[i + 2] = color.b;
-
-					i += 3;
-					j++;
-				}
-			}
-
-			particles.geometry.attributes.color.needsUpdate = true;
 			particles.material.uniforms.time.value = timeSinceStart;
 
 			renderer.render(scene, camera);
 		}
 
+		unsubscribe = animRadius.subscribe(
+			(value) => (particles.material.uniforms.animRadius.value = value)
+		);
+
 		dispatch('modelLoaded');
+
+		animRadius.set(maxDistanceFromCenter);
 	}
+
+	onDestroy(() => {
+		unsubscribe?.();
+	});
 
 	function mapRange(
 		value: number,
