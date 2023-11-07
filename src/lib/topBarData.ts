82fd6f5a54c2024dacb958e979662aafce5244b8
MXT <development@mxtreality.co.uk>
2023-11-07 12:34:57 +0000

    WEB-106: Merge task/WEB-106-add-colours-to-dots to main

--- src/lib/topBarData.ts
+++ src/lib/topBarData.ts
@@ -1,5 +1,5 @@
 import { writable, type Writable } from "svelte/store";
 
-export type PageId = 'home' | { caseStudies: 'root' | 'stonehenge' } | 'studio' | 'contacts';
+export type PageId = undefined | 'home' | { caseStudies: 'root' | 'stonehenge' } | 'studio' | 'contacts';
 
-export const navBarData: Writable<PageId> = writable('home');
+export const navBarData: Writable<PageId> = writable(undefined);
