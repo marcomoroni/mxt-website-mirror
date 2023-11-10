366719d538e159ba78312ea3177d3b708404015e
Marco Moroni <marcomoroni394@gmail.com>
2023-11-10 10:54:08 +0100

    WEB-108 better code

--- src/lib/three_vanilla/Dots.svelte
+++ src/lib/three_vanilla/Dots.svelte
@@ -30,6 +30,21 @@ out vec2 vDotIndex;
 out float vAccentColor1Noise;
 out float vAccentColor2Noise;
 
+const float rippleAnim1Fade = 2800.0;
+const float rippleAnim2DistFromAnim1 = 600.0;
+const float rippleAnim2Fade = 4700.0;
+
+const vec2 posXNoiseStartPos = vec2(3.0, -6.0);
+const vec2 posYNoiseStartPos = vec2(80.0, -30.0);
+const float posNoiseScale = 0.03;
+const float posNoiseTimeMult = 0.00006;
+const float posNoiseMult = 900.0;
+
+const vec2 scaleNoiseStartPos = vec2(0.0, 0.0);
+const float scaleNoiseScale = 0.03;
+const float scaleNoiseTimeMult = 0.0003;
+const float scaleNoiseMult = 20.0;
+
 ${noise3D}
 
 ${map}
@@ -38,26 +53,26 @@ ${quarticInOut}
 
 void main() {
 
-	float anim0to1 = clamp(map( distanceFromCenter - animRadius, 0.0, -2800.0, 0.0, 1.0), 0.0, 1.0);
-	anim0to1 = quarticInOut(anim0to1);
-	float anim0to1_2 = clamp(map( (distanceFromCenter - animRadius) + 600.0 , 0.0, -4700.0, 0.0, 1.0), 0.0, 1.0);
-	anim0to1_2 = quarticInOut(anim0to1_2);
+	float rippleAnim1 = clamp(map( distanceFromCenter - animRadius, 0.0, -rippleAnim1Fade, 0.0, 1.0), 0.0, 1.0); // 0 to 1
+	rippleAnim1 = quarticInOut(rippleAnim1);
+	float rippleAnim2 = clamp(map( (distanceFromCenter - animRadius) + rippleAnim2DistFromAnim1 , 0.0, -rippleAnim2Fade, 0.0, 1.0), 0.0, 1.0); // 0 to 1
+	rippleAnim2 = quarticInOut(rippleAnim2);
 
-	float posXNoise = cnoise(vec3(dotIndex.r * 0.03 + 3.0, dotIndex.g * 0.03 - 6.0, time * 0.00006));
-	float posYNoise = cnoise(vec3(dotIndex.r * 0.03 + 80.0, dotIndex.g * 0.03 - 30.0, time * 0.00006));
-	float posX = position.r + mix(0.0, posXNoise * 900.0, anim0to1_2);
-	float posY = position.g + mix(0.0, posYNoise * 900.0, anim0to1_2);
+	float posXNoise = cnoise(vec3(dotIndex.r * posNoiseScale + posXNoiseStartPos.r, dotIndex.g * posNoiseScale + posXNoiseStartPos.g, time * posNoiseTimeMult));
+	float posYNoise = cnoise(vec3(dotIndex.r * posNoiseScale + posYNoiseStartPos.r, dotIndex.g * posNoiseScale + posYNoiseStartPos.g, time * posNoiseTimeMult));
+	float posX = position.r + mix(0.0, posXNoise * posNoiseMult, rippleAnim2);
+	float posY = position.g + mix(0.0, posYNoise * posNoiseMult, rippleAnim2);
 	vec4 mvPosition = modelViewMatrix * vec4( posX, posY, 0.0, 1.0 );
 
-	float scaleNoise = cnoise(vec3(dotIndex.r * 0.03, dotIndex.g * 0.03, time * 0.0003));
+	float scaleNoise = cnoise(vec3(dotIndex.r * scaleNoiseScale + scaleNoiseStartPos.r, dotIndex.g * scaleNoiseScale + scaleNoiseStartPos.g, time * scaleNoiseTimeMult));
 	scaleNoise = map(scaleNoise, -1.0, 1.0, -0.4, 1.0);
-	float scale = mix(150.0, scaleNoise * 20.0, anim0to1);
+	float scale = mix(150.0, scaleNoise * scaleNoiseMult, rippleAnim1);
 	scale = scale * animVisibility;
 	gl_PointSize = scale;
 
 	gl_Position = projectionMatrix * mvPosition;
 
-	vAnim = anim0to1;
+	vAnim = rippleAnim1;
 	vDotIndex = dotIndex;
 	vAccentColor1Noise = posXNoise;
 	vAccentColor2Noise = posYNoise;
@@ -75,6 +90,9 @@ in float vAnim;
 in float vAccentColor1Noise;
 in float vAccentColor2Noise;
 
+// Before .r use the first colour, between .r and .g fade, and after .g use second colour.
+const vec2 colorGradientRange = vec2(0.6, 0.7);
+
 ${noise3D}
 
 ${map}
@@ -85,9 +103,8 @@ void main() {
 
 	float color1Noise = map(vAccentColor1Noise, -1.0, 1.0, 0.0, 1.0);
 	float color2Noise = map(vAccentColor2Noise, -1.0, 1.0, 0.0, 1.0);
-	// Accent colour only on heigher values.
-	color1Noise = map(color1Noise, 0.6, 0.7, 0.0, 1.0);
-	color2Noise = map(color2Noise, 0.6, 0.7, 0.0, 1.0);
+	color1Noise = map(color1Noise, colorGradientRange.r, colorGradientRange.g, 0.0, 1.0);
+	color2Noise = map(color2Noise, colorGradientRange.r, colorGradientRange.g, 0.0, 1.0);
 	color1Noise = clamp(color1Noise, 0.0, 1.0);
 	color2Noise = clamp(color2Noise, 0.0, 1.0);
 	vec3 color = mix( baseColor.rgb, accentColor1.rgb, color1Noise * vAnim );
