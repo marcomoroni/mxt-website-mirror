9b5786f52eac89574091e1d140754f13f50d5bca
Marco Moroni <marcomoroni394@gmail.com>
2024-01-29 14:59:44 +0100

    WEB-104 center

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,22 @@
 	<title>MXT</title>
 </svelte:head>
 
-<div>Coming soon</div>
+<div class="container">
+	<div class="margin" />
+	<div>Coming soon</div>
+	<div class="margin" />
+</div>
+
+<style>
+	.container {
+		width: 100%;
+		height: 100dvh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.margin {
+		flex: 1;
+	}
+</style>
