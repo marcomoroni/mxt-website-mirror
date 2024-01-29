96f208d71ab2a3ab890c15837c6c05d11f89be1b
Marco Moroni <marcomoroni394@gmail.com>
2024-01-29 14:51:09 +0100

    WEB-104 clean up

--- src/lib/gallery/GalleryItem.svelte
+++
@@ -1,8 +0,0 @@
-<figure class="item">
-	<div class="figure-container">
-		<slot name="figure" />
-	</div>
-	{#if $$slots.caption}
-		<figcaption><slot name="caption" /></figcaption>
-	{/if}
-</figure>
