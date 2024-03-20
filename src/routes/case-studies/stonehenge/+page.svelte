568a899c86ba117fe4cbd2d442dffd2fc8f14ddf
Marco Moroni <marco@mxtreality.co.uk>
2024-03-20 16:52:49 +0000

    WEB-94-A303-images

--- src/routes/case-studies/stonehenge/+page.svelte
+++ src/routes/case-studies/stonehenge/+page.svelte
@@ -11,7 +11,7 @@
 <CaseStudyLanding title={caseStudyData.title} leadParagraph={caseStudyData.leadParagraph} />
 <div class="landing-spacer" />
 <section class="intro-container">
-	<div class="img-stonehenge" />
+	<img class="img-stonehenge" src="/images/stonehenge.jpg" alt="Stonehenge." />
 	<div class="intro">
 		<p>
 			Stonehenge, one of the world's most iconic prehistoric landscapes, has been the focus of over
@@ -38,7 +38,6 @@
 			traditional presentations and illustrations was needed.
 		</p>
 	</div>
-	<div class="img-road" />
 </section>
 <div class="section-spacer" />
 <section class="section-results">
@@ -63,9 +62,42 @@
 			immersive spaces.
 		</p>
 	</div>
-	<div class="img-results-1" />
-	<div class="img-results-2" />
-	<div class="img-results-3" />
+	<div class="img-results-1">
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video autoplay loop muted controls class="video-redered-scenes">
+			<source src="/videos/A303_Rendered_Scenes.mp4" type="video/mp4" />
+		</video>
+	</div>
+	<img
+		class="img-results-2"
+		src="/images/HighresScreenshot00003.png"
+		alt="Stonenge in the digital environment."
+	/>
+	<img
+		class="img-results-3"
+		src="/images/HighresScreenshot00009.png"
+		alt="An entrance of the new A303 tunnel."
+	/>
+	<img
+		class="img-results-4"
+		src="/images/HighresScreenshot00007.png"
+		alt="Cars on the current A303 interecting a walking path."
+	/>
+	<img
+		class="img-results-5"
+		src="/images/HighresScreenshot00013.png"
+		alt="A roundabout at night."
+	/>
+	<img
+		class="img-results-6"
+		src="/images/HighresScreenshot00018.png"
+		alt="The proposed Green Bridge."
+	/>
+	<img
+		class="img-results-7"
+		src="/images/HighresScreenshot00027.png"
+		alt="The Countess Roundabout at night."
+	/>
 </section>
 <div class="section-spacer" />
 <section class="section-development">
@@ -119,8 +151,21 @@
 	</div>
 	<div class="gameplay-illustration-1 thin-border" />
 	<div class="gameplay-illustration-2 thin-border" />
-	<div class="gameplay-illustration-3" />
-	<div class="gameplay-illustration-4" />
+	<img
+		class="gameplay-illustration-3"
+		src="/images/HighresScreenshot00001.png"
+		alt="The controls in virtual reality."
+	/>
+	<img
+		class="gameplay-illustration-4"
+		src="/images/desktop_menu.jpg"
+		alt="The controls in desktop."
+	/>
+	<img
+		class="gameplay-illustration-5"
+		src="/images/HighresScreenshot00005.png"
+		alt="The environment seen from a high point with the Heritage Areas highlighted."
+	/>
 </section>
 <div class="section-spacer" />
 <section class="section-events">
@@ -162,12 +207,12 @@
 	.img-stonehenge {
 		grid-column: 1 / 7;
 		grid-row: 1 / 3;
-		border: 10px solid red;
 		height: max(500px, calc(100dvh - (var(--case-study-margin) * 2)));
 		position: sticky;
 		top: var(--case-study-margin);
 		left: 0;
 		align-self: start;
+		object-fit: cover;
 	}
 
 	.intro {
@@ -190,15 +235,8 @@
 	}
 
 	.type-2 {
-		grid-column: 3 / 7;
+		grid-column: 2 / 6;
 		grid-row: 1;
-	}
-
-	.img-road {
-		grid-column: 3 / 11;
-		grid-row: 2;
-		height: max(500px, calc(100dvh - (var(--case-study-margin) * 2)));
-		border: 10px solid salmon;
 	}
 
 	.section-results {
@@ -210,29 +248,47 @@
 	}
 
 	.type-results {
-		grid-column: 4 / 8;
+		grid-column: 6 / 10;
 		grid-row: 1;
 	}
 
 	.img-results-1 {
 		grid-column: 1 / 11;
 		grid-row: 2;
-		border: 10px solid greenyellow;
-		aspect-ratio: 16 / 9;
+	}
+
+	.video-redered-scenes {
+		width: 100%;
 	}
 
 	.img-results-2 {
 		grid-column: 2 / 10;
 		grid-row: 3;
-		border: 10px solid rebeccapurple;
-		aspect-ratio: 16 / 9;
 	}
 
 	.img-results-3 {
 		grid-column: 2 / 10;
 		grid-row: 4;
-		border: 10px solid aquamarine;
-		aspect-ratio: 16 / 9;
+	}
+
+	.img-results-4 {
+		grid-column: 2 / 10;
+		grid-row: 5;
+	}
+
+	.img-results-5 {
+		grid-column: 2 / 10;
+		grid-row: 6;
+	}
+
+	.img-results-6 {
+		grid-column: 2 / 10;
+		grid-row: 7;
+	}
+
+	.img-results-7 {
+		grid-column: 2 / 10;
+		grid-row: 8;
 	}
 
 	.section-development {
@@ -279,15 +335,16 @@
 	.gameplay-illustration-3 {
 		grid-column: 2 / 10;
 		grid-row: 3;
-		border: 10px solid rgba(124, 104, 238, 0.365);
-		aspect-ratio: 16 / 9;
 	}
 
 	.gameplay-illustration-4 {
 		grid-column: 2 / 10;
 		grid-row: 4;
-		border: 10px solid rgba(124, 104, 238, 0.365);
-		aspect-ratio: 16 / 9;
+	}
+
+	.gameplay-illustration-5 {
+		grid-column: 2 / 10;
+		grid-row: 5;
 	}
 
 	.section-events {
@@ -353,11 +410,6 @@
 			grid-row: 1;
 		}
 
-		.img-road {
-			grid-column: unset;
-			grid-row: 2;
-		}
-
 		.section-results {
 			grid-template-columns: unset;
 			grid-auto-flow: row;
@@ -381,6 +433,26 @@
 		.img-results-3 {
 			grid-column: unset;
 			grid-row: 4;
+		}
+
+		.img-results-4 {
+			grid-column: unset;
+			grid-row: 5;
+		}
+
+		.img-results-5 {
+			grid-column: unset;
+			grid-row: 6;
+		}
+
+		.img-results-6 {
+			grid-column: unset;
+			grid-row: 7;
+		}
+
+		.img-results-7 {
+			grid-column: unset;
+			grid-row: 8;
 		}
 
 		.section-development {
@@ -421,6 +493,11 @@
 		.gameplay-illustration-4 {
 			grid-column: unset;
 			grid-row: 5;
+		}
+
+		.gameplay-illustration-5 {
+			grid-column: unset;
+			grid-row: 6;
 		}
 
 		.section-events {
