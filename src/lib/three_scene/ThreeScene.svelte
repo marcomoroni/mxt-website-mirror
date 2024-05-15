448d8698267c964333ea9f73694b8afae216c8fc
Marco Moroni <marcomoroni394@gmail.com>
2024-05-15 14:45:44 +0200

    remove cad rotation

--- src/lib/three_scene/ThreeScene.svelte
+++ src/lib/three_scene/ThreeScene.svelte
@@ -196,8 +196,6 @@
 		}
 	];
 	const digitalInfrastructurePosY = 10;
-	const digitalInfrastructureRot = (opacity: number) =>
-		new THREE.Vector3(0, map(opacity, 0, 1, 9.5, 10), 0);
 	const servicesPropsData = [
 		{
 			baseColor: (currentBackgroundColor: string) => currentBackgroundColor,
@@ -212,14 +210,14 @@
 				{
 					mesh: '/models/DigitalInfrastructure.gltf',
 					ambientOcclusionTextureAndHighlight: '/models/DigitalInfrastructure_CADTexture.png',
-					rotationDisplacement: digitalInfrastructureRot,
+					rotationDisplacement: new THREE.Vector3(0, 0, 0),
 					positionDisplacement: (_opacity: number) =>
 						new THREE.Vector3(0, digitalInfrastructurePosY, 0)
 				},
 				...[0, 1, 2].map((i) => ({
 					mesh: '/models/CAD_Lines.gltf',
 					ambientOcclusionTextureAndHighlight: '/models/DigitalInfrastructure_CADTexture.png',
-					rotationDisplacement: digitalInfrastructureRot,
+					rotationDisplacement: new THREE.Vector3(0, 0, 0),
 					positionDisplacement: (
 						_opacity: number,
 						dioramaPositions: Array<{ x: number; z: number }>
