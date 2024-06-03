a188ec0412e2032a4b2bf21cc3e48454e92c9ec3
Marco Moroni <marcomoroni394@gmail.com>
2024-06-03 14:01:46 +0200

    WEB-179 remove hash navigation with

--- src/lib/servicesData.ts
+++ src/lib/servicesData.ts
@@ -1,20 +1,47 @@
-export const sectionsData = [
+export type SectionData = {
+	title: string;
+	subtitle: string;
+	href: string;
+	associatedState: 'service-1' | 'service-2' | 'service-3';
+};
+
+export const sectionsData: Array<SectionData> = [
 	{
 		title: 'Digital Infrastructure',
 		subtitle: 'Improving decision-making and engagement through multidisciplinary visualisations',
-		hash: '#digital-infrastructure',
+		href: '/services/digital-infrastructure',
 		associatedState: 'service-1' as 'service-1'
 	},
 	{
 		title: 'Driving Simulation',
 		subtitle: 'Behavioural science and road safety research',
-		hash: '#driving-simulation',
+		href: '/services/driving-simulation',
 		associatedState: 'service-2' as 'service-2'
 	},
 	{
 		title: 'Learning and Development',
 		subtitle: 'Interactive and immersive learning environments',
-		hash: '#interactive-and-immersive-learning-environments',
+		href: '/services/interactive-and-immersive-learning-environments',
 		associatedState: 'service-3' as 'service-3'
 	}
 ];
+
+// Note that if the url is '/services' we want it to be considered the first section
+// pageRouteId should be provided by Svelte's `$page.route.id`.
+export function getCurrentSectionData(
+	pageRouteId: string | null
+): { data: SectionData; index: number } | undefined {
+	let index: number | undefined = undefined;
+	if (pageRouteId === '/services') {
+		index = 0;
+	} else {
+		index = sectionsData.findIndex(({ href }) => href === pageRouteId);
+	}
+
+	const sectionData = sectionsData.at(index);
+	if (sectionData !== undefined) {
+		return { data: sectionData, index };
+	} else {
+		return undefined;
+	}
+}
