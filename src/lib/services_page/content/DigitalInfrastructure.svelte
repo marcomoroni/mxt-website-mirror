014302e0138fdd8c0a003e634284570cbbe8b20c
Marco Moroni <marco@mxt.co.uk>
2024-11-07 12:02:54 +0100

    WEB-214 digital infrastructure intro

--- src/lib/services_page/content/DigitalInfrastructure.svelte
+++ src/lib/services_page/content/DigitalInfrastructure.svelte
@@ -12,11 +12,11 @@
 			<Card intro={true}>
 				<div slot="front" class="intro">
 					<p>
-						MXT produce interactive visualisations for Nationally Significant Infrastructure
-						Projects (NSIPs). Understanding planning applications can be challenging. Our work is to
-						transform your existing data into intuitive and immersive experiences for diverse
-						stakeholders ranging from members of the public and policy makers to designers,
-						engineers and operational teams.
+						MXT produce interactive simulations for Nationally Significant Infrastructure Projects
+						(NSIPs). Understanding planning applications can be challenging. Our work is to
+						transform your existing data into intuitive and simulations for diverse stakeholders
+						ranging from members of the public and policy makers to designers, engineers and
+						operational teams.
 					</p>
 				</div>
 			</Card>
