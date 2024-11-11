709421f9986be3d3c817270c754da0c1c7ef3312
Marco Moroni <marco@mxt.co.uk>
2024-11-11 12:06:29 +0100

    typo

--- src/lib/services_page/content/DigitalInfrastructure.svelte
+++ src/lib/services_page/content/DigitalInfrastructure.svelte
@@ -14,9 +14,9 @@
 					<p>
 						MXT produce interactive simulations for Nationally Significant Infrastructure Projects
 						(NSIPs). Understanding planning applications can be challenging. Our work is to
-						transform your existing data into intuitive simulations for diverse stakeholders
-						ranging from members of the public and policy makers to designers, engineers and
-						operational teams.
+						transform your existing data into intuitive simulations for diverse stakeholders ranging
+						from members of the public and policy makers to designers, engineers and operational
+						teams.
 					</p>
 				</div>
 			</Card>
@@ -141,7 +141,7 @@
 								<p>
 									Transport schemes are designed to make customer journeys safer, greener, quicker,
 									and more reliable. The most impactful way of communicating those benefits is to
-									show those customers how their journey’s will change.
+									show those customers how their journeys will change.
 								</p>
 								<p>
 									MXT is the first and currently the only UK government provider to combine building
