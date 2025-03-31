cbeee3ba10f78fa6697c19148ab20689cb4542af
Marco Moroni <marcomoroni394@gmail.com>
2025-03-31 14:21:31 +0200

    gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-        adapter: adapter()
-	},
+		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
